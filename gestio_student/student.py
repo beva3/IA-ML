258ce64357d7c3c6283ac948e933fb61ce810ca1
beva3 <mizabienvenu@gmail.com>
2024-10-21 23:06:03 +0530

    get somme

--- gestio_student/student.py
+++ gestio_student/student.py
@@ -1,16 +1,8 @@
 from traiteents import *
 
 
+print(update_data(data))
 
-data = {
-    'name' : ['Nanja','Valih','Tsanta'],
-    'attendence':[12,23,21],
-    'assignment':[12,34,21],
-    'project':[4,5,6],
-    'exam':[12,15,7],
-    'is_pass':[True,False,True]
-}
 
-df = pd.DataFrame(data)
 
-print(df)
+
--- gestio_student/traiteents.py
+++ gestio_student/traiteents.py
@@ -1,1 +1,32 @@
 import pandas as pd
+
+data = {
+    'name' : ['Nanja','Valih','Tsanta'],
+    'attendence':[12,23,21],
+    'assignment':[12,34,21],
+    'project':[4,5,6],
+    'exam':[12,15,7],
+    'somme':[0,0,0],
+    'is_pass':[True,False,True]
+}
+
+MAX_REQUIR={
+    'at':5,
+    'as':25,
+    'proj':40,
+    'ex':30
+}
+
+def is_pass(note_student):
+    if note_student > 0.5*pd.DataFrame(MAX_REQUIR).sum(axis=1):
+        return True
+    else : return False
+
+def update_data(data_student):
+    print("update")
+    df = pd.DataFrame(data_student)
+    df['somme'] = df[['attendence', 'assignment', 'project', 'exam']].sum(axis=1)
+    
+    
+
+    return df
